9b3db3a9d1c47bac09af98191b0bb529c6ba0287
karthi0023d <karthi0023d@gmail.com>
2023-07-08 21:04:27 +0530

    string_m.java file added - 08_commit

---
+++ string_m.java
@@ -0,0 +1,10 @@
+class Methods{
+	public static String m1(){
+		String s="Welocome to java";
+		return s;
+	}
+	public static void main(String[] args){
+		String s=m1();
+		System.out.println(s);
+	}
+}
