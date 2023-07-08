a79569a16d626fc695d77239ace87d778f2e2510
karthi0023d <93076444+karthi0023d@users.noreply.github.com>
2023-07-08 22:56:51 +0530

    Update Array_.java

--- Array_.java
+++ Array_.java
@@ -2,7 +2,8 @@ class test{
 	 public static void main(String[] args){
         int[] i={1,3,5,6,8};
 		for(int j:i){
-			System.out.print(j+"\t");
+			System.out.print(j); // changes made
+			System.out.println(""\t); // new line added
 		}
     }
 }
