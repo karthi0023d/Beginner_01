b8a5417972b27d0b7970e387b0137a0aea3c5834
karthi0023d <karthi0023d@gmail.com>
2023-07-08 23:07:56 +0530

    updated Array_.java - 11_commit

--- Array_.java
+++ Array_.java
@@ -1,8 +1,10 @@
 class test{
-	 public static void main(String[] args){
-        int[] i={1,3,5,6,8};
+	public static void main(String[] args){
+        	System.out.println("int array program");
+		int[] i={1,3,5,6,8};
 		for(int j:i){
 			System.out.print(j+"\t");
+			System.out.print("\r");
 		}
     }
 }
