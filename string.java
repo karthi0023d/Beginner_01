83198df4ffcbee750d85abbdc45eec331d693ff7
karthi0023d <karthi0023d@gmail.com>
2023-07-08 15:40:43 +0530

    string.java file modified - 04_commit

--- string.java
+++ string.java
@@ -10,6 +10,20 @@ class Demo{
 		Arrays.sort(ch);
 		String s=String.valueOf(ch);
 		System.out.println(s);
+
+		String str="DCSAE";
+		char[] chr=str.toCharArray();
+		char temp;
+		for(int i=0;i<str.length();i++){
+			for(int j=i+1;j<str.length();j++){
+				if(chr[i] > chr[j]){
+					temp=chr[i];
+					chr[i]=chr[j];
+					chr[j]=temp;
+				}
+			}
+			System.out.print(chr[i]);
+		}
 	}
 }
 
