6dc4e82e6b9dd365d696c0ebd1adad8c5acbba77
karthi0023d <karthi0023d@gmail.com>
2023-07-08 22:35:46 +0530

    new method added in string_m.java - 09_commit

--- string_m.java
+++ string_m.java
@@ -1,10 +1,26 @@
+import java.util.*;
 class Methods{
+	static String s="Welcome to java";
 	public static String m1(){
-		String s="Welocome to java";
 		return s;
 	}
+	public static String m2(){
+		String str="";
+		for(int i=0;i<s.length();i++){
+			if(s.charAt(i)== 'a'|s.charAt(i)=='e'|s.charAt(i)=='o'|s.charAt(i)=='i'|s.charAt(i)=='u'){
+				continue;
+			}else{
+				str=str+s.charAt(i);
+			}
+		}
+		return str;
+	}
+		
 	public static void main(String[] args){
-		String s=m1();
-		System.out.println(s);
+		String s1=m1();
+		System.out.println(s1);
+		String s2=m2();
+		//System.out.println(s2); - Wlcm t jv
+		System.out.println(m2()); // - Wlcm t jv
 	}
 }
