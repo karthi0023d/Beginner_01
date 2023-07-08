6de1618db2521d856762c7aa42db03e5f8d45eca
karthi0023d <karthi0023d@gmail.com>
2023-07-08 15:13:37 +0530

    string.java file added-02_commit

---
+++ string.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[] args){
+		String s="CDWSF";
+		char[] ch=s.toCharArray();
+		for(char c:ch){
+			System.out.print(c);
+		}
+		System.out.println();
+	}
+}
